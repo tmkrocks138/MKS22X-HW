0000755af56ed76671acb03d11aff79fc043b1fe
tmkrocks138 <tkhanna@stuy.edu>
2017-02-15 15:09:47 -0500

    did nothing but formatting...yay

---
+++ hw03-KnightBoard/KnightBoard.java
@@ -0,0 +1,21 @@
+public class KnightBoard{
+    private int[][] board;
+    public KnightBoard(int startingRows,int startingCols){
+	board = new int [startingRows][startingCols];
+    }
+	
+    public String toString(){
+	return "";
+    } //blank if you never called solve or when there is no solution
+
+    public void solve(){} 
+
+    private boolean solveH(int row ,int col, int level){
+	return false;
+    }
+
+ // level is the # of the knight
+
+
+
+}
