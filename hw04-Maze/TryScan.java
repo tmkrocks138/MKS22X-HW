e4d5face71f3a0e0385ec45128df5d0bc85a4d72
tmkrocks138 <tkhanna@stuy.edu>
2017-03-02 15:34:55 -0500

    STUFF

---
+++ hw04-Maze/TryScan.java
@@ -0,0 +1,11 @@
+import java.util.*;//random, scanner, arrayList
+import java.io.*;//file, filenotfourd
+public class TryScan{
+    public static void main(String[] args){
+	Scanner in = new Scanner("HELLO! This\nis a block\nof text!");
+//	while(in.hasNext()){
+	    System.out.println(in.nextLine());
+//	}
+    } 
+
+}
