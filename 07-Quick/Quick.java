2e9650867c492df1a3c5a921833750c221231229
tmkrocks138 <tkhanna@stuy.edu>
2017-03-16 15:34:31 -0400

    AHHHHHHH 2.0

--- 07-Quick/Quick.java
+++ 07-Quick/Quick.java
@@ -27,18 +27,22 @@ public class Quick{
 
     public static int part(int[] ary, int start, int end){
 
-	int ans = -1;
 	
 	Random r = new Random();
 	int specialI = r.nextInt(end+1 - start)+start;
 	int val = ary[specialI];
 	
-	//System.out.println(val);
+	System.out.println(val);
 
 	int[] temp= new int[ary.length];
+
 	int low = start, high = end;
-	for(int i = start; i < end + 1; i++){
-	    if(!(i == specialI)){
+
+	for(int i = start; i < end; i++){
+
+            System.out.println(ary[i]);
+
+	    if((i!=specialI) &&  (i!=end)){
 		if(ary[i] < val){
 		    temp[low]=ary[i];
 		    low++;
@@ -48,24 +52,29 @@ public class Quick{
 		    high--; 
 		}
 	    }
+	    if(i==end){
+		specialI = low;
+		temp[low]=val;
+	    }
 	}
-	if(low == high){
-	    ans = low;
-	    temp[low] = val;
-	}
-	ary = temp;
 	
-	return ans;
+
+	for(int i = 0; i < ary.length; i++){
+	    ary[i]=temp[i];
+	}
+	return specialI;
     }
 
 
     public static void main(String[] args){
 	int[]ary = { 2, 10, 15, 23, 0,  5};
-	System.out.println(select( ary , 0 ));
-	System.out.println(select( ary , 1 ));
-	System.out.println(select( ary , 2 ));
-	System.out.println(select( ary , 3 ));
-	System.out.println(select( ary , 4 ));
-	System.out.println(select( ary , 5 ));
+	System.out.println(part( ary, 0, ary.length - 1 ));
+//	System.out.println(select( ary , 0 ));
+//	System.out.println(select( ary , 1 ));
+//	System.out.println(select( ary , 2 ));
+//	System.out.println(select( ary , 3 ));
+//	System.out.println(select( ary , 4 ));
+//	System.out.println(select( ary , 5 ));
     } 
 }
+
