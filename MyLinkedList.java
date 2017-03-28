4f5178f62b0ce16dedd158dfefc4ec7bf83dd279
“tmkrocks138” <tmkrocks138@gmail.com>
2017-03-28 01:17:14 -0400

    Blah

---
+++ MyLinkedList.java
@@ -0,0 +1,31 @@
+public class MyLinkedList{
+    public LNode start, end;
+    public int size;
+
+    public void add(){
+
+	//while(this.next!=/*null*/){}
+	/*Create a new LNode with a value
+	  redirects the last term to the new block-->set next to null and previous to end*/
+
+
+    }
+
+    public String toString(){
+	String str = "";
+	/*start at start, tack on every value each node. when next == null, stop*/
+	return str;
+
+    }
+
+
+
+
+
+    public class LNode{
+	public int value;
+	public LNode next, previous;
+    }
+    
+}
+
---
+++ MyLinkedList.java~
@@ -0,0 +1,26 @@
+public class MyLinkedList{
+    public LNode start, end;
+    public int size;
+
+    public void add(){
+
+	//while(this.next!=/*null*/){}
+	/*Create a new LNode with a value
+	  redirects the last term to the new block-->set next to null and previous to end*/
+
+
+    }
+
+    public String toString(){}
+
+
+
+
+
+    public class LNode{
+	public int value;
+	public LNode next, previous;
+    }
+    
+}
+
