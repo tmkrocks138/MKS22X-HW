e5c4324db4fa7629565f5b7e159af4a32e6cd1a6
“tmkrocks138” <tmkrocks138@gmail.com>
2017-04-21 08:25:02 -0400

    Problem with Double parce

---
+++ FolderForNoReason/PostfixMath.java
@@ -0,0 +1,46 @@
+
+import java.util.*;
+import java.lang.*;
+
+public class PostfixMath{
+
+    public double eval(String s){
+	String[] tokens = s.split(" ");
+	Stack<Double> values = new Stack <Double>();
+	for (int i =0; i < tokens.length; i++){
+	    if(isOp(tokens[i])){
+		values.push(apply(tokens[i],values.pop(),values.pop()));
+	    }
+	    else{
+		values.push(Double.parceDouble(tokens[i]));
+	    }
+	}
+    }
+
+    public boolean isOp(String s){
+	return s.equals("+")
+	    || s.equals("-")
+	    || s.equals("*")
+	    || s.equals("/")
+	    || s.equals("%");
+    }
+
+    public Double apply(String s, Double b, Double c){
+	if(s.equals("+")){
+	    return b + c;
+	}
+	if(s.equals("-")){
+	    return c - b;
+	}
+	if(s.equals("*")){
+	    return c * b;
+	}
+	if(s.equals("/")){
+	    return c / b;
+	}
+        if(s.equals("%")){
+	    return c % b;
+	}
+	return 0.0;
+    }
+}
