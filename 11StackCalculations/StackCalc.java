38681079ed1529c5bce8c888edc4225f96520f30
“tmkrocks138” <tmkrocks138@gmail.com>
2017-04-25 00:29:46 -0400

    I DONT KNOW WHATS WRONG BUT IT PARCEDOUBLE IS WRONG UGH

---
+++ 11StackCalculations/StackCalc.java
@@ -0,0 +1,53 @@
+
+import java.util.*;
+import java.lang.*;
+
+public class StackCalc{
+
+    public double eval(String s){
+	String[] tokens = s.split(" ");
+	Stack<Double> values = new Stack <Double>();
+	for (int i =0; i < tokens.length; i++){
+	    if(isOp(tokens[i])){
+		values.push(apply(tokens[i],values.pop(),values.pop()));
+	    }
+	    else{
+		try{
+		Double temp = Double.parceDouble(tokens[i]);
+		values.push(temp);
+		}
+		catch(IllegalArgumentException e){
+		    System.out.println("WRONG");
+		}
+		
+	    }
+	}
+    }
+
+    public boolean isOp(String s){
+	return s.equals("+")
+	    || s.equals("-")
+	    || s.equals("*")
+	    || s.equals("/")
+	    || s.equals("%");
+    }
+
+    public Double apply(String s, Double b, Double c){
+	if(s.equals("+")){
+	    return b + c;
+	}
+	if(s.equals("-")){
+	    return c - b;
+	}
+	if(s.equals("*")){
+	    return c * b;
+	}
+	if(s.equals("/")){
+	    return c / b;
+	}
+        if(s.equals("%")){
+	    return c % b;
+	}
+	return 0.0;
+    }
+}
