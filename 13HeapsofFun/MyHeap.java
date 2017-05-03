d1650de6805671bcb29bb381f9b7907fa835c450
tmkrocks138 <tkhanna@stuy.edu>
2017-05-03 15:34:25 -0400

    AHHHHH

---
+++ 13HeapsofFun/MyHeap.java
@@ -0,0 +1,14 @@
+public class MyHeap{
+    private int size;
+    private String[] heap;
+    
+    public MyHeap(){
+	heap = new String[10];
+	size=0;
+    }
+
+    private void resize(){
+	String[] temp = new String[heap.length + 1]
+    }
+
+}
