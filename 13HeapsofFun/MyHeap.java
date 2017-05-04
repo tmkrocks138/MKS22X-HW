d9965c0d78a0f22508da5bb74f918dc448cb5bc2
tmkrocks138 <tkhanna@stuy.edu>
2017-05-04 15:34:08 -0400

    :)

--- 13HeapsofFun/MyHeap.java
+++ 13HeapsofFun/MyHeap.java
@@ -1,14 +1,44 @@
 public class MyHeap{
     private int size;
-    private String[] heap;
+    private ArrayList<String> heap;
     
     public MyHeap(){
-	heap = new String[10];
+	heap = new ArrayList<String>;
+	heap.add("NOTHING!!");
 	size=0;
     }
 
-    private void resize(){
-	String[] temp = new String[heap.length + 1]
+    public void add(String s){
+	
+	heap.add(s);
+	size++;
+	heap.pushUp();
+    }
+
+    public String remove(){
+	String ans = heap.get(1);
+	heap.remove(1);
+	heap.add(1, heap.remove(size));
+	size--;
+	heap.pushDown();
+	return ans;
+    }
+
+    private void pushUp(){
+	int index = size;
+	String move = heap.get(size);
+	String s;
+	while(move.compareTo)
+    }
+
+    private void pushDown(){
+	int index = 1;
+
+    }
+
+
+    public String peek(){
+	return heap.get(1);
     }
 
 }
