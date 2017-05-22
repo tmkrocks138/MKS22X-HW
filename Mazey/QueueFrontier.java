403e30b38e3d3a6d3f4237fcccb6cee3df5556d5
“tmkrocks138” <tmkrocks138@gmail.com>
2017-05-22 11:33:57 -0400

    STUFF

--- Mazey/QueueFrontier.java
+++ Mazey/QueueFrontier.java
@@ -1,4 +1,35 @@
-public class QueueFrontier implements Frontier{
 
+import java.util.*;
+
+public class QueueFrontier implements Frontier{
+    private LinkedList<Location> listy;
+    private int size;
+    
+    public QueueFrontier(){
+	listy = new LinkedList<Location>();
+    }
+    
+    public int getSize(){
+	return size;
+    }
+
+    public void add(Location l){
+	listy.add(l);
+	size++;
+    }
+
+    public boolean hasNext(){
+	return listy.peekFirst() != null;
+    }
+    
+    public Location next(){
+	if (hasNext()){
+	    size--;
+	    return listy.remove();
+	}
+	else{
+	    return null;
+	}
+    }
 
 }
